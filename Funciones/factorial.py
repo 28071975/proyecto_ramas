e92f3f36351d0cadea5ec5552cd07735f77a39bb
Javier Martinez <javier.martinez910@unisalamanca.edu.co>
2025-11-25 20:41:22 -0500

    factorial de los primeros 10 numeros

--- Funciones/factorial.py
+++ Funciones/factorial.py
@@ -1,1 +1,9 @@
 # funciones/factorial.py
+def factorial(n):
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
+    return resultado
+
+for i in range(1, 11):
+    print(f"Factorial de {i} = {factorial(i)}")
